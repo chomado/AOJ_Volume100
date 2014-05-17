916c2cafd0f045f0897c2ba24e632205dcf05f50
ちょまど <chomado@gmail.com>
2014-05-17 15:15:04 +0900

    Update p19_Sum_of_Numbers.c

--- p19_Sum_of_Numbers.c
+++ p19_Sum_of_Numbers.c
@@ -18,9 +18,12 @@ int main(void)
        /* 初期化 */
         memset(cNum, '\0', MAX);
         for (i=0; i<MAX; i++) num[i] = 0;
+        /* 文字列として読み込む */
         scanf("%s", cNum);
+        /* 0 だけの入力だったら終了 */
         if (!(strcmp(cNum, "0"))) break;
-        sum = 0;
+        
+        sum = 0; // init
         char_to_int(cNum, num);
         for (i=0; i<strlen(cNum); i++)
             sum += num[i];
